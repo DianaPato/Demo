ac2d985603d48f033d763264ffc8897595ff4fc4
Diana P. Gazquez <dianagazquez@gmail.com>
2016-04-25 12:39:58 -0300

    Adding first test entity

---
+++ model/src/main/java/com/igneo/model/admin/AdminUser.java
@@ -0,0 +1,20 @@
+package com.igneo.model.admin;
+
+/**
+ * @author diana
+ *
+ */
+public class AdminUser {
+
+	public AdminUser() {
+		this.name = "Diana";
+		this.lastName ="Gazquez";
+		this.password = "aaa";
+				
+	}
+	
+	private String name;
+	private String  lastName;
+	private String  password;
+	
+}
